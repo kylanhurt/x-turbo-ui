81472731aa861d1ccac97c4323db95e0320de3aa
Kylan Hurt <kylan.hurt@gmail.com>
2024-06-21 19:08:03 -0500

    Acquiring usernames working except initial batch

--- src/content/xHome.ts
+++ src/content/xHome.ts
@@ -20,47 +20,41 @@ const getUsernameFromNode = (node) => {
 
 }
 
-const config = { attributes: true, childList: true, subtree: true };
+const config = { attributes: false, childList: true, subtree: false };
 
 let timeline: HTMLElement | null = null
 let timelineMountInterval = setInterval(() => {
   timeline = document.querySelector('[aria-label="Timeline: Your Home Timeline"]')
   if (timeline) {
-    console.log('timeline', timeline)
     clearInterval(timelineMountInterval)
     watchTimelineForNewTweets()
   }
 }, 1000)
 
-const getUsernameOfTweet = (tweet) => {
-  const username = tweet.querySelector('[data-testid="User-Name"]').textContent
+const getUsernameOfTweet = (tweetNode) => {
+  const userNameArea = tweetNode.querySelector('[data-testid="User-Name"]')
+  // console.log('userNameArea', userNameArea)
+  const username = userNameArea.children[1].children[0].children[0].querySelector('span').textContent.replace('@', '')
+  console.log('username', username)
+  // const username = userNameArea.textContent
   return username
 
 }
 
 const watchTimelineForNewTweets = () => {
-  console.log('watchTimelineForNewTweets', timeline)
   if (!timeline) return
   const targetNode = timeline.firstChild as HTMLElement
 
-  console.log('targetNode: ', targetNode)
+  let lastNode
   const callback = (mutationList, observer) => {
     for (const mutation of mutationList) {
-      if (mutation.type === "childList") {
-        // console.log('recheck children length', targetNode.children.length)
-        const children = targetNode.children
-        // get username of each tweet
-        const usernameArray = []
-        for (let i = 0; i < children.length; i++) {
-          const tweet = children[i]
-          const username = getUsernameOfTweet(tweet)
-          usernameArray.push(username)
-        }
-        console.log('usernameArray', usernameArray)
-        // console.log("A child node has been added or removed.");
-      } /* else if (mutation.type === "attributes") {
-        console.log(`The ${mutation.attributeName} attribute was modified.`);
-      } */
+      lastNode = mutation.addedNodes[0]
+      // get username of each tweet
+      const usernameArray = []
+      for (const tweet of mutation.addedNodes) {
+        const username = getUsernameOfTweet(tweet)
+        usernameArray.push(username)
+      }
     }
   };
   
@@ -70,30 +64,3 @@ const watchTimelineForNewTweets = () => {
   // Start observing the target node for configured mutations
   observer.observe(targetNode, config);
 }
-
-// addEventListener('load', function() {
-//   const targetNode = document.querySelector("body");
-//   console.log('targetNode: ', targetNode)
-//   console.log('load')
-//   // find all Tweets in DOM
-//   let tweets = document.querySelectorAll('[data-testid="tweet"]')
-//   console.log('tweets', tweets)
-//   let userNames = document.querySelectorAll('[data-testid="User-Name"]')
-//   console.log('userNames', userNames)
-
-//   const callback = (mutationList, observer) => {
-//     for (const mutation of mutationList) {
-//       if (mutation.type === "childList") {
-//         console.log("A child node has been added or removed.");
-//       } else if (mutation.type === "attributes") {
-//         console.log(`The ${mutation.attributeName} attribute was modified.`);
-//       }
-//     }
-//   };
-  
-//   // Create an observer instance linked to the callback function
-//   const observer = new MutationObserver(callback);
-  
-//   // Start observing the target node for configured mutations
-//   observer.observe(targetNode, config);
-// })
