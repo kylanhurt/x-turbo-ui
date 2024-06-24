3e6c540b1680eff4d4e1316a92cc38faf455241b
Kylan Hurt <kylan.hurt@gmail.com>
2024-06-24 17:10:06 -0500

    Add shadcn stylized Button and Input

--- src/components/UserNoteInput.svelte
+++ src/components/UserNoteInput.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { Input } from '$lib/components/ui/input/index.js';
+  import { Button } from "$lib/components/ui/button";
   let inputValue = '';
 
   function handleSubmit(e: Event) {
@@ -8,7 +10,8 @@
   }
 </script>
 
-<form on:submit={handleSubmit}>
-  <input type="text" bind:value={inputValue} placeholder="Enter your note" />
-  <button type="submit">Submit</button>
-</form>
+<div class="twp">
+  <form on:submit={handleSubmit} class='user-note-input'>
+    <Input type="text" bind:value={inputValue} placeholder="Enter your note" class="max-w-xs" />
+  </form>
+</div>
